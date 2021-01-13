2d2ab8f04bbcf75ae1e9d0616005f0c4dabb595c
Zach <zdspringer99@gmail.com>
2021-01-12 23:28:57 -0800

    completed employee.js test

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,37 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+
+  printStats() {
+    console.log(`Name: ${this.name}`);
+    console.log(`ID: ${this.id}`);
+    console.log(`Email: ${this.email}`);
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getRole() {
+    return "Employee";
+  }
+}
+// const newEmp = new Employee("bob", 1, "joe@joe.joe");
+// console.log(newEmp);
+// newEmp.getName();
+module.exports = Employee;
