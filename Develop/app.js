1216f8fc5bd0e7aad43d26f79b9240eae37aaeee
Zach <zdspringer99@gmail.com>
2021-01-13 20:59:45 -0800

    successfully able to create team.html

--- Develop/app.js
+++ Develop/app.js
@@ -10,105 +10,126 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const finalArray = [];
+
 function generateTeam() {
   inquirer
     .prompt([
       {
-        type: "input",
-        message: "Employee Name:",
-        name: "name",
-        default: "Name of Employee",
-      },
-      {
-        type: "input",
-        message: "Employee Email:",
-        name: "email",
-        default: "email@email.com",
-      },
-      {
-        type: "input",
-        message: "Employee ID:",
-        name: "id",
-        default: "1,2,3,4,5",
-      },
-      {
         type: "list",
-        message: "Employee Role:",
-        choices: [Manager, Engineer, Intern],
-        name: "role",
-        default: "Manager, Engineer, Intern",
+        message: "Finished adding members to your team?",
+        name: "out",
+        choices: ["YES", "NO"],
       },
     ])
-    .then((primus) => {
-      // primus being the first user input
-      if (primus.role.Manager === true) {
+    .then((confirm) => {
+      if (confirm.out === "NO") {
         inquirer
           .prompt([
             {
               type: "input",
-              message: "Office Number:",
-              name: "officeNum",
-              default: "###",
+              message: "Employee Name:",
+              name: "name",
+              default: "Name of Employee",
             },
-          ])
-          .then((secundus) => {
-            const manager = new Manager(
-              primus.name,
-              primus.id,
-              primus.email,
-              secundus.officeNum
-            );
-          });
-      } else if (primus.role.Engineer === true) {
-        inquirer
-          .prompt([
             {
               type: "input",
-              message: "GitHub User:",
-              name: "github",
-              default: "GitHubUser",
+              message: "Employee Email:",
+              name: "email",
+              default: "email@email.com",
             },
-          ])
-          .then((tertius) => {
-            const engineer = new Engineer(
-              primus.name,
-              primus.id,
-              primus.email,
-              tertius.github
-            );
-          });
-      } else if (primus.role.Intern === true) {
-        inquirer
-          .prompt([
             {
               type: "input",
-              message: "School and Degree:",
-              name: "school",
-              default: "Graduate of SchoolName with a DegreeName",
+              message: "Employee ID:",
+              name: "id",
+              default: "1,2,3,4,5",
+            },
+            {
+              type: "list",
+              message: "Employee Role:",
+              choices: [Manager, Engineer, Intern],
+              name: "role",
+              default: "Manager, Engineer, Intern",
             },
           ])
-          .then((quartus) => {
-            const intern = new Intern(
-              primus.name,
-              primus.id,
-              primus.email,
-              quartus.school
-            );
+          .then((data) => {
+            if (data.role === "Manager") {
+              inquirer
+                .prompt([
+                  {
+                    type: "input",
+                    message: "Office Number:",
+                    name: "officeNum",
+                    default: "###",
+                  },
+                ])
+                .then((manage) => {
+                  const manager = new Manager(
+                    data.name,
+                    data.id,
+                    data.email,
+                    manage.officeNum
+                  );
+                  finalArray.push(manager);
+                  generateTeam();
+                });
+            } else if (data.role === "Engineer") {
+              inquirer
+                .prompt([
+                  {
+                    type: "input",
+                    message: "GitHub User:",
+                    name: "github",
+                    default: "GitHubUser",
+                  },
+                ])
+                .then((engine) => {
+                  const engineer = new Engineer(
+                    data.name,
+                    data.id,
+                    data.email,
+                    engine.github
+                  );
+                  finalArray.push(engineer);
+                  generateTeam();
+                });
+            } else if (data.role === "Intern") {
+              inquirer
+                .prompt([
+                  {
+                    type: "input",
+                    message: "School and Degree:",
+                    name: "school",
+                    default: "Graduate of SchoolName with a DegreeName",
+                  },
+                ])
+                .then((student) => {
+                  const intern = new Intern(
+                    data.name,
+                    data.id,
+                    data.email,
+                    student.school
+                  );
+                  finalArray.push(intern);
+                  generateTeam();
+                });
+            }
           });
       } else {
-        console.log("Please Select Employee Role");
-        generateTeam();
+        produceHTML(render(finalArray));
       }
     });
 }
 // After the user has input all employees desired, call the `render` function (required
-
 generateTeam();
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
+const produceHTML = (savingString) => {
+  fs.writeFile("./output/team.html", savingString, (err) => {
+    if (err) throw err;
+    console.log(`File successfully saved`);
+  });
+};
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
